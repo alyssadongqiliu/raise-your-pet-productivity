a6d0b8e4cb261cf709cbe287464175001d141367
alyssa <dongql@unc.edu>
2025-06-27 01:48:37 -0400

    Update App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <h1>Task List</h1>
+    <TaskList />
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue'
+export default {
+  components: { TaskList }
+}
+</script>
