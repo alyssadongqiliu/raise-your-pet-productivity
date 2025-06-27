c171f4bf1f943bd65ec241aa999d5f6f4269f9fe
alyssa <dongql@unc.edu>
2025-06-27 01:55:46 -0400

    Update urls.py

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,10 +1,13 @@
+# backend/app/urls.py
+
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet
 
 router = DefaultRouter()
-router.register(r'tasks', TaskViewSet, basename='task')
+router.register('tasks', TaskViewSet, basename='task')
 
+# MUST be a list
 urlpatterns = [
     path('', include(router.urls)),
 ]
